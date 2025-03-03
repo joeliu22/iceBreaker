85dc97e7ca857a21f0dad5f1ab043b2bd4246fa1
Joey Liu <zliu2513@gmail.com>
2025-03-03 11:48:56 -0500

    Initial Commit

---
+++ iceBreaker/iceBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iceBreakerApp.swift
+//  iceBreaker
+//
+//  Created by ZHANGYU LIU on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct iceBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iceBreakerTests/iceBreakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  iceBreakerTests.swift
+//  iceBreakerTests
+//
+//  Created by ZHANGYU LIU on 3/3/25.
+//
+
+import Testing
+@testable import iceBreaker
+
+struct iceBreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
